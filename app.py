16e9533a2a232f11eff201a3d93449cf6a5bd79b
Kiran Kumar Chaudhary <kirankr24@outlook.com>
2023-04-19 10:15:22 +0100

    update

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 from flask import Flask, request, jsonify
-import pickle
+import joblib
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 
-# Load the saved XGBoost model
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# Load the trained model
+model = joblib.load("model.pkl")
 
-@app.route('/predict', methods=['POST'])
+# Initialize the encoders (use the same encoders from the training script)
+energy_type_encoder = LabelEncoder()
+energy_consuming_sector_encoder = LabelEncoder()
+
+# Fit the encoders (use the same categories from the training script)
+energy_type_encoder.fit(["Coal", "Electricity", "Gas", "Bioenergy & Wastes", "Petroleum Products", "Manufactured Fuels"])
+energy_consuming_sector_encoder.fit(["Rail", "Domestic", "Public Sector", "Industrial & Commercial", "Agriculture", "Road Transport", "Commercial", "Industrial"])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    # Get the input features from the request
-    data = request.get_json(force=True)
-    input_features = pd.DataFrame(data, index=[0])
+    input_data = request.get_json()
 
-    # Make predictions using the trained XGBoost model
-    predictions = model.predict(input_features)
+    # Preprocess the input features before making predictions
+    input_data["Energy Type"] = energy_type_encoder.transform([input_data.get("Energy Type")])[0]
+    input_data["Energy Consuming Sector"] = energy_consuming_sector_encoder.transform([input_data.get("Energy Consuming Sector")])[0]
 
-    # Return the predicted energy consumption value
-    return jsonify(predictions[0])
 
-if __name__ == '__main__':
+    # Convert input data to a DataFrame
+    input_df = pd.DataFrame([input_data])
+
+    # Make the prediction
+    prediction = model.predict(input_df)[0]
+
+    # Return the prediction as JSON
+    return jsonify({"prediction": prediction})
+
+if __name__ == "__main__":
     app.run(debug=True)
